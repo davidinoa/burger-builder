0cdc5ddf24e0d143d780e4c31d72174030f45aa9
David Inoa <davidinoa@gmail.com>
2018-02-05 11:10:05 -0500

    Implement logic to show modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -21,7 +21,8 @@ class BurgerBuilder extends Component {
       meat: 0
     },
     totalPrice: 4,
-    purchasable: false
+    purchasable: false,
+    purchasing: false
   }
 
   updatePurchaseState (ingredients) {
@@ -62,13 +63,17 @@ class BurgerBuilder extends Component {
 
     const priceDeduction = INGREDIENT_PRICES[type];
     const oldPrice = this.state.totalPrice;
-    const newPrice = oldPrice + priceDeduction;
+    const newPrice = oldPrice - priceDeduction;
 
     this.setState({
       totalPrice: newPrice,
       ingredients: updatedIngredients
     });
     this.updatePurchaseState(updatedIngredients);
+  }
+
+  purchaseHandler () {
+    this.setState({ purchasing: true });
   }
 
   render () {
@@ -81,7 +86,7 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
-        <Modal>
+        <Modal show={this.state.purchasing}>
           <OrderSummary ingredients={this.state.ingredients} />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
@@ -90,6 +95,7 @@ class BurgerBuilder extends Component {
           ingredientRemoved={this.removeIngredientHandler} 
           disabled={disabledInfo}
           purchasable={this.state.purchasable}
+          ordered={this.purchaseHandler.bind(this)}
           price={this.state.totalPrice} />
       </Aux>
     );
