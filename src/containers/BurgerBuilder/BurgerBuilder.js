760b8346069c96aaeb4730fb6e93b2c75d266fea
David Inoa <davidinoa@gmail.com>
2018-02-04 20:38:20 -0500

    Add button to place order

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,7 +18,16 @@ class BurgerBuilder extends Component {
       cheese: 0,
       meat: 0
     },
-    totalPrice: 4
+    totalPrice: 4,
+    purchasable: false
+  }
+
+  updatePurchaseState (ingredients) {
+    const sum = Object.keys(ingredients)
+      .map(ingredient => ingredients[ingredient])
+      .reduce((sum, element) => sum + element, 0);
+
+    this.setState({ purchasable: sum > 0});
   }
 
   addIngredientsHandler = (type) => {
@@ -37,6 +46,7 @@ class BurgerBuilder extends Component {
       totalPrice: newPrice,
       ingredients: updatedIngredients
     });
+    this.updatePurchaseState(updatedIngredients);
   }
 
   removeIngredientHandler = (type) => {
@@ -56,6 +66,7 @@ class BurgerBuilder extends Component {
       totalPrice: newPrice,
       ingredients: updatedIngredients
     });
+    this.updatePurchaseState(updatedIngredients);
   }
 
   render () {
@@ -73,6 +84,7 @@ class BurgerBuilder extends Component {
           ingredientAdded={this.addIngredientsHandler}
           ingredientRemoved={this.removeIngredientHandler} 
           disabled={disabledInfo}
+          purchasable={this.state.purchasable}
           price={this.state.totalPrice} />
       </Aux>
     );
