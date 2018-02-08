a4faf7bfe56e32d6a99b07fdbf395b9e5ed6475c
David Inoa <davidinoa@gmail.com>
2018-02-08 10:18:36 -0500

    Navigate to checkout page from builder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -91,29 +91,30 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'David Inoa',
-        address: {
-          street: '16 Main St',
-          zipCode: '10001',
-          country: 'US'
-        },
-        email: 'davidinoa@email.com'
-      },
-      deliveryMethod: 'fastest'
-    }
+    // this.setState({ loading: true });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'David Inoa',
+    //     address: {
+    //       street: '16 Main St',
+    //       zipCode: '10001',
+    //       country: 'US'
+    //     },
+    //     email: 'davidinoa@email.com'
+    //   },
+    //   deliveryMethod: 'fastest'
+    // }
 
-    axios.post('/orders.json', order)
-      .then(response => {
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch(error => {
-        this.setState({ loading: false, purchasing: false });
-      });
+    // axios.post('/orders.json', order)
+    //   .then(response => {
+    //     this.setState({ loading: false, purchasing: false });
+    //   })
+    //   .catch(error => {
+    //     this.setState({ loading: false, purchasing: false });
+    //   });
+    this.props.history.push('/checkout');
   }
 
   render () {
