9fb45ffdf09575ef939d9993a6d6739623678527
David Inoa <davidinoa@gmail.com>
2018-02-10 08:29:24 -0500

    Dynamically create inputs based on config

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -82,12 +82,22 @@ class ContactData extends Component {
   }
 
   render () {
+    const formElementsArray = [];
+    for (let key in this.state.orderForm) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key]
+      })
+    }
     let form = (
       <form>
-        <Input elementType='...' elementConfig='...' value='...' />
-        <Input inputtype='input' type='email' name='email' placeholder='Email' />
-        <Input inputtype='input' type='text' name='street' placeholder='Street' />
-        <Input inputtype='input' type='text' name='zip' placeholder='Zip Code' />
+        {formElementsArray.map(formElement => (
+          <Input
+            key={formElement.id}
+            elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value} />
+        ))}
         <Button buttonType='Success' clicked={this.orderHandler}>Order</Button>
       </form>
     );
