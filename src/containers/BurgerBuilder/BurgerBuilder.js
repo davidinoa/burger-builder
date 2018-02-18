1d1eeddfd4614457d12f5be30cdbb9e215de628a
David Inoa <davidinoa@gmail.com>
2018-02-17 21:47:03 -0500

    Redirect unauthenticated users from burger builder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,8 +27,12 @@ class BurgerBuilder extends Component {
     return sum > 0;
   }
 
-  purchaseHandler () {
-    this.setState({ purchasing: true });
+  purchaseHandler = () => {
+    if (this.props.isAuthenticated) {
+      this.setState({ purchasing: true });
+    } else {
+      this.props.history.push('/auth');
+    }
   }
 
   purchaseCancelHandler = () => {
@@ -61,7 +65,8 @@ class BurgerBuilder extends Component {
             ingredientRemoved={this.props.onIngredientRemoved} 
             disabled={disabledInfo}
             purchasable={this.updatePurchaseState(this.props.ings)}
-            ordered={this.purchaseHandler.bind(this)}
+            ordered={this.purchaseHandler}
+            isAuth={this.props.isAuthenticated}
             price={this.props.price} />
         </Aux>
       );
@@ -89,7 +94,8 @@ const mapStateToProps = state => {
   return {
     ings: state.burgerBuilder.ingredients,
     price: state.burgerBuilder.totalPrice,
-    error: state.burgerBuilder.error
+    error: state.burgerBuilder.error,
+    isAuthenticated: state.auth.token !== null
   };
 }
 
